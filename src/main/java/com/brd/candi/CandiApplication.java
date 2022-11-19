883fe78d0a66ad2eead726683cd5ea52bf68f78b
Yan <yannpassaro@gmail.com>
2022-11-19 18:53:45 -0300

    Initial commit

---
+++ src/main/java/com/brd/candi/CandiApplication.java
@@ -0,0 +1,13 @@
+package com.brd.candi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brd/candi/CandiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brd.candi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
